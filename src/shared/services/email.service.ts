f4ed38ec748b66f26e8108d2b14e570ac1aeea23
Thunggg <thuannguyen20041028@gmail.com>
2026-02-07 19:47:03 +0700

    feat: use html template

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import fs from 'fs';
+import path from 'path';
 import { Resend } from 'resend';
 import envConfig from '../config';
 
@@ -11,11 +13,19 @@ export class EmailService {
   }
 
   async sendOTP(payload: { email: string; code: string }) {
+    const otpTemplate = fs.readFileSync(
+      path.resolve('src/shared/email-template/otp.html'),
+      'utf-8',
+    );
+
+    console.log(otpTemplate);
     return await this.resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: [`${payload.email}`],
       subject: 'Verify email',
-      html: `<strong>${payload.code}</strong>`,
+      html: otpTemplate
+        .replaceAll('{{code}}', payload.code)
+        .replaceAll('{{to}}', payload.email),
     });
   }
 }
