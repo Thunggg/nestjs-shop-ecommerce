5e14926a1caa020612eb7bd9c1db8ec818034dbb
Thunggg <thuannguyen20041028@gmail.com>
2026-02-07 20:48:15 +0700

    feat: use react email to send mail with react template

--- src/shared/services/email.service.tsx
+++ src/shared/services/email.service.tsx
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import fs from 'fs';
-import path from 'path';
+import AWSVerifyEmail from 'emails/aws-verify-email';
+import * as React from 'react';
 import { Resend } from 'resend';
 import envConfig from '../config';
 
@@ -13,19 +13,11 @@ export class EmailService {
   }
 
   async sendOTP(payload: { email: string; code: string }) {
-    const otpTemplate = fs.readFileSync(
-      path.resolve('src/shared/email-template/otp.html'),
-      'utf-8',
-    );
-
-    console.log(otpTemplate);
     return await this.resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: [`${payload.email}`],
       subject: 'Verify email',
-      html: otpTemplate
-        .replaceAll('{{code}}', payload.code)
-        .replaceAll('{{to}}', payload.email),
+      react: <AWSVerifyEmail verificationCode={payload.code} />,
     });
   }
 }
